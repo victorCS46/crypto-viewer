063027947a8da9de7a36cad99e53ced6ad3a4172
Victor Cruz <victorcruzsuero@gmail.com>
2023-09-22 15:54:02 -0400

    Set beta folder structure

---
+++ src/app/cryptocurrencies/[crypto]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Crypto(props: any) {
+  return (
+    <div>Crypto {JSON.stringify(props)}</div>
+  )
+}
---
+++ src/app/cryptocurrencies/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CryptocurrenciesLayout(props: any) {
+  return (
+    <section>
+      {props.children}
+    </section>
+  )
+}
